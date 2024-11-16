d186aec2267e3c81a978bb0d74d1c8ac0a6f2d19
luisangelf11 <71238653+luisangelf11@users.noreply.github.com>
2024-11-16 13:26:23 -0400

    Net money card

--- src/app/dashboard/components/DashboardPanel.tsx
+++ src/app/dashboard/components/DashboardPanel.tsx
@@ -27,6 +27,11 @@ export default function DashboardPanel() {
     });
     return total;
   }, []);
+
+  //Net money
+  const netMoney = useMemo(() => {
+    return incomes - expenses;
+  }, [expenses, incomes]);
   return (
     <article className="flex w-full justify-center items-center gap-4 mt-4">
       <Card title="Expenses" value={expenses}>
@@ -58,6 +63,22 @@ export default function DashboardPanel() {
             strokeLinecap="round"
             strokeLinejoin="round"
             d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z"
+          />
+        </svg>
+      </Card>
+      <Card title="net money" value={netMoney}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-10"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
           />
         </svg>
       </Card>
